e7d15c2c37db1400008768b2e6750cc86a082418
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-07-28 19:00:01 -0500

    Implemented puitDb function

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,14 @@ const initdb = async () =>
     });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+    console.log('Post into db');
+    const connectDb = await openDB('jate', 1);
+    const tx = connectDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const result = await store.add({content: content});
+    console.log('Data saved', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
